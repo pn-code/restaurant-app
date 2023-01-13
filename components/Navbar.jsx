c5f7eea00fc0db1dc6731d4c7736b07105de1b01
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-13 12:50:24 -0800

    Added styles and content to Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { BiPhoneCall, BiCart } from "react-icons/bi";
 
 const Navbar = () => {
-  return (
-    <div>
-        Navbar
-    </div>
-  )
-}
+    return (
+        <div className="flex sticky top-0 items-center justify-between h-[100px] py-0 px-12 bg-slate-600 text-white ">
+            {/* Call Section */}
+            <div className="flex flex-1">
+                <BiPhoneCall className="mt-2" size={32} />
 
-export default Navbar
+                <div className="font-bold ml-5">
+                    <div>ORDER NOW</div>
+                    <div className="text-xl">123 456 7890</div>
+                </div>
+            </div>
+
+            {/* Center */}
+            <div className="flex-[3]">
+                <ul className="flex m-5 font-semibold text-lg justify-between">
+                    <li className="navList">Home</li>
+                    <li className="navList">Products</li>
+                    <li className="navList">Menu</li>
+                    <Image
+                        src="/assets/logo.png"
+                        alt=""
+                        width={160}
+                        height={70}
+                    />
+                    <li className="navList">Events</li>
+                    <li className="navList">Blog</li>
+                    <li className="navList">Contact</li>
+                </ul>
+            </div>
+
+            {/* Links */}
+            <div className="flex flex-1 justify-end">
+                <div className="relative">
+                    <BiCart size={36} />
+                    <div className="flex items-center justify-center absolute -top-2 -right-2 w-5 h-5 rounded-full bg-slate-800 p-2 font-semibold text-sm">
+                        2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
