ea488c63658235cb72eeb88608faf88e5c88889a
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-16 21:13:40 -0800

    Added Responsive Design: Footer & Product Card

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,35 +4,35 @@ import React from "react";
 const Footer = () => {
     return (
         // Container
-        <div className="h-[calc(100vh-116px)] bg-gray-900 flex mt-12">
+        <div className="h-auto sm:h-[calc(100vh-116px)] bg-gray-900 flex mt-12">
             {/* Image */}
-            <div className="flex-1 relative">
+            <div className="hidden sm:flex flex-1 relative">
               {/* Photo by Ronan Kruithof <a href="https://unsplash.com/@ronank?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ronan Kruithof</a> on <a href="https://unsplash.com/photos/PCE0T5i4pDI?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>*/}
                 <Image src="/assets/contact.jpg" layout="fill" objectFit="cover" alt="" />
             </div>
 
             {/* Contact */}
-            <div className="flex-2 relative text-gray-300 flex p-12 justify-between">
+            <div className="flex flex-col sm:flex-row flex-2 relative text-gray-300 p-12 justify-between text-center sm:text-left">
                 {/* Motto */}
-                <div className="flex flex-1 py-0">
-                    <h1 className="text-[32px] font-bold text-gray-100">ALWAYS SERVING THE BEST FOODS</h1>
+                <div className="flex flex-1 pb-10 sm:py-0">
+                    <h1 className="text-[48px] sm:text-[32px] font-bold text-gray-100">ALWAYS SERVING THE BEST FOODS</h1>
                 </div>
                 {/* Locations */}
-                <div className="flex-2 py-0 px-5">
-                    <h1 className="text-[18px] text-[#b7903c] font-bold pb-5">FIND OUR RESTAURANTS</h1>
-                    <p className="pb-5">
+                <div className="flex-2 py-0 sm:px-5">
+                    <h1 className="text-[32px] sm:text-[18px] text-[#b7903c] font-bold pb-5">FIND OUR RESTAURANTS</h1>
+                    <p className="text-[24px] sm:text-[16px] pb-5">
                         123 Addy Way,
                         <br />
                         Notacity, 98765
                         <br /> (123) 456-7890
                     </p>
-                    <p className="pb-5">
+                    <p className="text-[24px] sm:text-[16px] pb-5">
                         1234 Addy Way,
                         <br />
                         Notacity, 98765
                         <br /> (123) 456-7890
                     </p>
-                    <p className="pb-5">
+                    <p className="text-[24px] sm:text-[16px] pb-5">
                         12345 Addy Way,
                         <br />
                         Notacity, 98765
@@ -40,13 +40,13 @@ const Footer = () => {
                     </p>
                 </div>
                 {/* Hours */}
-                <div className="flex-1 py-0 px-5">
-                    <h1 className="text-[18px] text-[#b7903c] font-bold pb-5">WORKING HOURS</h1>
-                    <p>
+                <div className="flex-1 py-0 sm:px-5">
+                    <h1 className="text-[32px] sm:text-[18px] text-[#b7903c] font-bold pb-5">WORKING HOURS</h1>
+                    <p className="text-[24px] sm:text-[16px] pb-5">
                         MONDAY THROUGH FRIDAY
                         <br /> 9:00 - 24:00
                     </p>
-                    <p>
+                    <p className="text-[24px] sm:text-[16px] pb-5">
                         MONDAY THROUGH FRIDAY
                         <br /> 12:00 - 21:00
                     </p>
--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const ProductCard = () => {
     return (
         // Container
-        <div className="w-[20%] flex flex-col items-center justify-center py-5 px-10">
+        <div className="w-[100%] sm:w-[50%] md:w-[33%] xl:w-[20%] flex flex-col items-center justify-center py-5 px-10">
             {/* Product Image */}
             <Image
                 src="/assets/featured1.png"
@@ -14,15 +14,15 @@ const ProductCard = () => {
             />
 
             {/* Product Name */}
-            <h1 className="text-[18px] font-bold text-slate-600">
+            <h1 className="text-[32px] md:text-[24px] font-bold text-slate-600">
                 Product Name
             </h1>
 
             {/* Price */}
-            <span className="text-[18px] font-bold text-slate-500">Price</span>
+            <span className="text-[28px] md:text-[20px] font-bold text-slate-500">Price</span>
 
             {/* Description */}
-            <p className="text-center text-slate-400">
+            <p className="text-[24px] md:text-[16px] text-center text-slate-400">
                 Lorem ipsum dolor sit amet, consectetur adipiscing
                 elit.
             </p>
