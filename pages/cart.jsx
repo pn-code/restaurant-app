975bb7c5e3fd7d82a8cb15cc82532f418ecd868b
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-16 16:17:22 -0800

    Added Right-side portion of cart component

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 const Cart = () => {
     return (
         // Container
-        <div className="p-12">
+        <div className="p-12 flex">
             {/* Left */}
-            <table className="w-[100%] flex-2 border-spacing-5">
+            <table className="w-[100%] flex-[2]">
                 <tr>
                     <th>Product</th>
                     <th>Name</th>
@@ -43,14 +43,30 @@ const Cart = () => {
                     <td>1</td>
                     {/* Total */}
                     <td>
-                        <span className="font-semibold text-lg">
-                            $18.00
-                        </span>
+                        <span className="font-semibold text-lg">$18.00</span>
                     </td>
                 </tr>
             </table>
             {/* Right */}
-            <div className="flex-1"></div>
+            <div className="flex-1">
+                {/* Wrapper */}
+                <div className="flex flex-col w-[90%] justify-between p-14 pt-[16px] max-h-[300px] rounded-md bg-gray-800 text-white">
+                    <h2 className="font-bold text-xl my-2">CART TOTAL</h2>
+                    {/* Subtotal Text */}
+                    <div>
+                        <b className="mr-[10px]">Subtotal: </b> $100.00
+                    </div>
+                    {/* Discount Text */}
+                    <div>
+                        <b className="mr-[10px]">Discount:</b> $0.00
+                    </div>
+                    {/* Total Text */}
+                    <div>
+                        <b className="mr-[10px]">Total:</b> $100.00
+                    </div>
+                    <button className="h-8 text-slate-600 font-bold cursor-pointer bg-white mt-6 rounded-md hover:scale-105 ease-out duration-150">Checkout</button>
+                </div>
+            </div>
         </div>
     );
 };
