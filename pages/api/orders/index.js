f3c2068d9a201d10828b89e2ae1ff30a5a1f3d9c
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-18 12:01:37 -0800

    Completed GET/POST API endpoint

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -2,13 +2,28 @@ import dbConnect from "@/lib/dbConnect";
 import Order from "../../../models/Order";
 
 export default async function handler(req, res) {
-    const { method, query:{id} } = req;
+    const {
+        method,
+        query: { id },
+    } = req;
 
     dbConnect();
 
     if (method === "GET") {
+        try {
+            const orders = await Order.find()
+            res.status(201).json(orders);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 
     if (method === "POST") {
+        try {
+            const order = await Order.create(req.body);
+            res.status(201).json(order);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 }
