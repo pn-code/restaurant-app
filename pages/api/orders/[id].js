35869f8c01d83902898a9c7fe501890813a7468a
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-18 12:27:12 -0800

    Changed id.js to [id].js and added GET endpoint

---
+++ pages/api/orders/[id].js
@@ -0,0 +1,34 @@
+import dbConnect from "@/lib/dbConnect";
+import Order from "../../../models/Order";
+
+export default async function handler(req, res) {
+    const {
+        method,
+        query: { id },
+    } = req;
+
+    dbConnect();
+
+    if (method === "GET") {
+        try {
+            const order = await Order.findById(id);
+            res.status(200).json(order);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+
+    if (method === "PUT") {
+        try {
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+
+    if (method === "DELETE") {
+        try {
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
