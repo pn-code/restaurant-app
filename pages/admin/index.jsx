1d012af45a284930b1d79e4f83ebfa6b5e0d34bc
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-19 11:46:18 -0800

    Added CSS styles

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const index = () => {
     return (
         // Container
-        <div>
+        <div className="p-12 flex">
             {/* Products */}
-            <div>
-                <h1>Products</h1>
-                <table>
+            <div className="flex-1">
+                <h1 className="text-2xl font-bold mb-2">Products</h1>
+                <table className="w-[100%] border-spacing-5 text-left">
                     <tbody>
                         <tr>
                             <th>Image</th>
@@ -19,21 +19,22 @@ const index = () => {
                     </tbody>
                     <tbody>
                         <tr>
+                            <td>Image</td>
                             <td>Product Id</td>
                             <td>Product Title</td>
                             <td>Product Price</td>
                             <td>
-                                <button>Edit</button>
-                                <button>Delete</button>
+                                <button className="px-2 py-1 bg-green-600 text-white rounded-md font-semibold">Edit</button>
+                                <button className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md font-semibold">Delete</button>
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
             {/* Orders */}
-            <div>
-                <h1>Orders</h1>
-                <table>
+            <div className="flex-1">
+                <h1 className="text-2xl font-bold mb-2">Orders</h1>
+                <table className="w-[100%] border-spacing-5 text-left">
                     <tbody>
                         <tr>
                             <th>Order #</th>
@@ -46,15 +47,15 @@ const index = () => {
                     </tbody>
                     <tbody>
                         <tr>
-                            <td>213213124</td>
+                            <td>{"213213124".slice(0, 5)}...</td>
                             <td>John Doe</td>
                             <td>$50</td>
                             <td>Paid</td>
                             <td>Preparing</td>
                             <td>Action</td>
                             <td>
-                                <button>Next Stage</button>
-                                <button>Cancel</button>
+                                <button className="px-2 py-1 bg-blue-600 text-white rounded-md font-semibold">Next Stage</button>
+                                <button className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md font-semibold">Cancel</button>
                             </td>
                         </tr>
                     </tbody>
