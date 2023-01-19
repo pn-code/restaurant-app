edfc17257d615e1db281132aa4edbd65d8a04b8b
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-19 12:02:08 -0800

    Added Server-side Rendering

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import axios from "axios";
+import Image from "next/image";
 
-const index = () => {
+const index = ({ orders, products }) => {
+
     return (
         // Container
         <div className="p-12 flex">
@@ -11,24 +14,38 @@ const index = () => {
                     <tbody>
                         <tr>
                             <th>Image</th>
-                            <th>Id</th>
+                            <th>Product Id</th>
                             <th>Title</th>
                             <th>Price</th>
                             <th>Action</th>
                         </tr>
                     </tbody>
-                    <tbody>
-                        <tr>
-                            <td>Image</td>
-                            <td>Product Id</td>
-                            <td>Product Title</td>
-                            <td>Product Price</td>
-                            <td>
-                                <button className="px-2 py-1 bg-green-600 text-white rounded-md font-semibold">Edit</button>
-                                <button className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md font-semibold">Delete</button>
-                            </td>
-                        </tr>
-                    </tbody>
+
+                    {products.map((product) => (
+                        <tbody key={products._id}>
+                            <tr>
+                                <td>
+                                    <Image
+                                        src={product.image}
+                                        alt=""
+                                        width={50}
+                                        height={50}
+                                    />
+                                </td>
+                                <td>{product._id.slice(0, 8)}...</td>
+                                <td>{product.title}</td>
+                                <td>${product.prices[0]}</td>
+                                <td>
+                                    <button className="px-2 py-1 bg-green-600 text-white rounded-md font-semibold">
+                                        Edit
+                                    </button>
+                                    <button className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md font-semibold">
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    ))}
                 </table>
             </div>
             {/* Orders */}
@@ -45,20 +62,26 @@ const index = () => {
                             <th>Action</th>
                         </tr>
                     </tbody>
-                    <tbody>
-                        <tr>
-                            <td>{"213213124".slice(0, 5)}...</td>
-                            <td>John Doe</td>
-                            <td>$50</td>
-                            <td>Paid</td>
-                            <td>Preparing</td>
-                            <td>Action</td>
-                            <td>
-                                <button className="px-2 py-1 bg-blue-600 text-white rounded-md font-semibold">Next Stage</button>
-                                <button className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md font-semibold">Cancel</button>
-                            </td>
-                        </tr>
-                    </tbody>
+                    {orders.map((order) => (
+                        <tbody key={order._id}>
+                            <tr>
+                                <td>{order._id.slice(0, 5)}...</td>
+                                <td>{order.customer}</td>
+                                <td>${order.total}</td>
+                                <td>{order.method === 1 ? "Paypal" : "Cash"}</td>
+                                <td>{order.status}</td>
+                                <td>Action</td>
+                                <td>
+                                    <button className="px-2 py-1 bg-blue-600 text-white rounded-md font-semibold">
+                                        Next Stage
+                                    </button>
+                                    <button className="ml-2 px-2 py-1 bg-red-500 text-white rounded-md font-semibold">
+                                        Cancel
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    ))}
                 </table>
             </div>
         </div>
@@ -66,3 +89,15 @@ const index = () => {
 };
 
 export default index;
+
+export const getServerSideProps = async () => {
+    const productRes = await axios.get(`http://localhost:3000/api/products`);
+    const orderRes = await axios.get(`http://localhost:3000/api/orders`);
+
+    return {
+        props: {
+            products: productRes.data,
+            orders: orderRes.data,
+        },
+    };
+};
