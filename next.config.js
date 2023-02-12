dc84f6f145fd0488249e38571d70af7673d8e6fa
Philip Nguyen <philipnguyen953@gmail.com>
2023-02-11 20:00:55 -0800

    Adding main api path to address CORS policy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains:["res.cloudinary.com"]
+  },
+  rewrites: async function() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://restaurant-app-phi.vercel.app:path*'
+      }
+    ]
   }
 }
 
