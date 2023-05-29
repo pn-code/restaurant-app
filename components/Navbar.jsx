089109922eb06f69894b49756848ddd82daafbf9
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-29 15:01:38 -0700

    Restyled Navbar with new logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,20 +8,20 @@ const Navbar = () => {
     const quantity = useSelector((state) => state.cart.quantity);
 
     return (
-        <div className="flex z-50 sticky top-0 items-center justify-between h-[100px] py-0 px-12 bg-slate-600 text-white ">
+        <div className="flex z-50 sticky top-0 items-center justify-between h-[80px] py-0 px-12 bg-white text-black shadow-xl">
             {/* Call Section */}
-            <div className="flex flex-[3] md:flex-1 hover:text-[#d7905c] ease-in duration-200">
+            <div className="flex flex-[3] md:flex-1 hover:text-gray-700 ease-in duration-200 cursor-pointer">
                 <BiPhoneCall className="mt-2" size={32} />
 
-                <div className="font-bold ml-5">
+                <div className="font-bold ml-5 text-sm">
                     <div>ORDER NOW</div>
-                    <div className="text-xl">123 456 7890</div>
+                    <div className="text-[18px]">123 456 7890</div>
                 </div>
             </div>
 
             {/* Center */}
             <div className="hidden md:block justify-between flex-[3]">
-                <ul className="flex m-5 font-semibold text-lg justify-between">
+                <ul className="flex m-5 font-bold text-lg justify-between">
                     <li className="navList">
                         <Link href="/" passHref>
                             Home
@@ -30,10 +30,11 @@ const Navbar = () => {
                     <li className="navList">Menu</li>
                     <Link href="/" passHref>
                         <Image
+                        className="rounded-md"
                             src="/assets/logo.png"
                             alt=""
-                            width={160}
-                            height={70}
+                            width={240}
+                            height={160}
                         />
                     </Link>
 
@@ -45,9 +46,9 @@ const Navbar = () => {
             {/* Cart */}
             <div className="flex flex-1 justify-end">
                 <Link href="/cart" passHref>
-                    <div className="relative hover:scale-110 ease-in duration-200 hover:text-[#d7905c]">
+                    <div className="relative hover:scale-110 ease-in duration-200 hover:text-gray-700 text-black">
                         <BiCart size={36} />
-                        <div className="flex items-center justify-center absolute -top-2 -right-2 w-5 h-5 rounded-full bg-slate-800 p-2 font-semibold text-sm">
+                        <div className="flex items-center justify-center absolute -top-2 -right-2 w-5 h-5 rounded-full bg-slate-800 p-2 font-semibold text-sm text-white">
                             {quantity}
                         </div>
                     </div>
