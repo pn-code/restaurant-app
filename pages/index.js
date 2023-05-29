b8d33d9c741d44998ba10a6d68abbc7853cfe4aa
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-29 14:39:58 -0700

    Updated favicon, title, & description

--- pages/index.js
+++ pages/index.js
@@ -6,36 +6,34 @@ import axios from "axios";
 const host = process.env.HOST_URI + "/api/products";
 
 export default function Home({ productList, admin }) {
-    return (
-        <>
-            <Head>
-                <title>Restaurant</title>
-                <meta name="description" content="restaurant app" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Featured />
-            <ProductList productList={productList} admin={admin}/>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Monster Burger</title>
+        <meta name="description" content="This Restaurant App is a full-stack Next.js application that allows users to browse the menu, customize orders with add-ons and meal combos, view and manage their cart, place orders with PayPal or cash, and track order status." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+      <Featured />
+      <ProductList productList={productList} admin={admin} />
+    </>
+  );
 }
 
 export const getServerSideProps = async (ctx) => {
-    const res = await axios.get(host);
-    const myCookie = ctx.req?.cookies || "";
-    let admin = false;
+  const res = await axios.get(host);
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
 
-    if (myCookie.token === process.env.TOKEN) {
-        admin = true;
-    }
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
 
-    return {
-        props: {
-            productList: res.data,
-            admin
-        },
-    };
+  return {
+    props: {
+      productList: res.data,
+      admin,
+    },
+  };
 };
