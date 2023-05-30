6825c8f4d040cd808008de6f42d5306f4c501a28
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-29 21:42:50 -0700

    Updated Cart styles

--- components/CartCard.jsx
+++ components/CartCard.jsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 const CartCard = ({ product }) => {
     return (
-        <tbody>
+        <tbody className="text-sm border-b-2 border-gray-300">
             <tr className="cartItem">
                 {/* Product */}
-                <td className="flex-[2]">
+                <td className="flex-1">
                     <div className="w-[100px] h-[100px] relative">
                         <Image
                             src={product.image}
@@ -17,40 +17,40 @@ const CartCard = ({ product }) => {
                     </div>
                 </td>
                 {/* Product Name */}
-                <td className="flex-[1]">
-                    <span className="font-semibold text-slate-600 text-3xl sm:text-lg">
+                <td className="flex-[1] pt-10">
+                    <span className="font-semibold text-slate-600">
                         {product.title}
                     </span>
                 </td>
                 {/* Extras */}
-                <td className="flex-[1]">
-                    <p className="flex flex-col text-slate-600 text-xl sm:text-[12px] font-semibold">
+                <td className="flex-[2] pt-10">
+                    <p className="flex flex-col text-slate-600 font-semibold">
                         {product.extraOptions.map((option) => (
                             <span key={option}>{option}</span>
                         ))}
                     </p>
                 </td>
                 {/*  Product Combo */}
-                <td className="flex-[1]">
-                    <ul className="text-xl sm:text-[18px]">
+                <td className="flex-[1] pt-10">
+                    <ul>
                         <li>{product.side}</li>
                         <li>{product.drink}</li>
                     </ul>
                 </td>
                 {/* Product Price */}
-                <td className="flex-[1]">
+                <td className="flex-[1] pt-10">
                     <p>
                         <span className="sm:hidden">PRICE: </span>$
                         {product.price}
                     </p>
                 </td>
                 {/* Product Quantity */}
-                <td className="flex-[1]">
+                <td className="flex-[1] pt-10">
                     <span className="sm:hidden">COUNT: </span>
                     {product.quantity}
                 </td>
                 {/* Total */}
-                <td className="flex-[1]">
+                <td className="flex-[1] pt-10">
                     <p className="text-2xl sm:text-lg font-bold">
                         <span className="sm:hidden">TOTAL: </span>$
                         {product.price * product.quantity}
