62d6e2fb7976f084ec6389b7456321e5102868ab
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-29 23:00:43 -0700

    Added order now button to navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
-import { BiPhoneCall, BiCart } from "react-icons/bi";
+import { BiCart } from "react-icons/bi";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
@@ -32,14 +32,15 @@ const Navbar = () => {
         </Link>
 
         {/* Cart */}
-        <section className="flex flex-1 justify-end xl:justify-center items-center">
+        <section className="flex flex-1 justify-end xl:justify-center items-center gap-6">
+          <Link className="hidden sm:flex text-xl font-bold border-4 border-black p-2 hover:text-white hover:bg-black ease-linear duration-200" href="/menu">ORDER NOW</Link>
           <Link
-            className="rounded-md flex items-center bg-gray-900 text-white h-12 px-4 group hover:bg-gray-800/90"
+            className="flex items-center bg-gray-900 text-white p-3 group hover:bg-gray-800/90"
             href="/cart"
             passHref
           >
             <div className="relative hover:text-gray-700 text-black">
-              <BiCart size={30} color="white" />
+              <BiCart size={28} color="white" />
               <div className="flex items-center justify-center absolute -top-1 -right-1 w-4 h-4 rounded-full bg-red-500 p-2 font-semibold text-sm text-white">
                 {quantity}
               </div>
