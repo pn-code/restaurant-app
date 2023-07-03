2a9a741cfbe0882fa2c57b2b9d1564da640d6324
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-02 21:10:06 -0700

    Made mobile navbar hidden on larger screens

--- components/MobileNavbar.jsx
+++ components/MobileNavbar.jsx
@@ -4,7 +4,7 @@ import { BiCart, BiFoodMenu } from "react-icons/bi";
 
 export default function MobileNavbar({ quantity }) {
     return (
-        <nav className="flex justify-around items-center fixed left-0 bottom-0 w-full bg-gray-100 border-t-2 border-t-white">
+        <nav className="sm:hidden flex justify-around items-center fixed left-0 bottom-0 w-full bg-gray-100 border-t-2 border-t-white">
             <Link href="/menu">
                 <div className="mt-1 flex flex-col items-center justify-center">
                     <BiFoodMenu size={16} />
