d09e3348041c5b2ba3529c297c6009630a6a55c7
Philip Nguyen <philipnguyen953@gmail.com>
2023-08-31 12:47:26 -0700

    Removed description & added Add btn

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -3,53 +3,40 @@ import Image from "next/image";
 import Link from "next/link";
 
 const ProductCard = ({ product }) => {
-  return (
-    // Container
-    <div className="bg-white rounded-sm hover:shadow-xl hover:bg-gray-200 ease-in duration-300 w-[100%] sm:w-[50%] md:w-[33%] xl:w-[20%] flex flex-col items-center justify-center py-5 px-10">
-      <Link
-        className="flex flex-col justify-center items-center"
-        href={`/products/${product._id}`}
-      >
-        {/* Product Image */}
-        <section className="w-[200px] h-[200px] flex justify-center items-center">
-          <Image
-            className="rounded-md"
-            src={product.image}
-            alt={product.title}
-            width={500}
-            height={500}
-          />
-        </section>
+    return (
+        // Container
+        <div className="bg-gray-50 rounded-md shadow-md border-2 border-gray-300 w-[100%] sm:w-[50%] md:w-[33%] xl:w-[20%] flex flex-col items-center justify-center py-5 px-10">
+            {/* Product Image */}
+            <div className="flex flex-col gap-2">
+                <section className="w-[200px] h-[200px] flex justify-center items-center">
+                    <Image
+                        className="rounded-md"
+                        src={product.image}
+                        alt={product.title}
+                        width={500}
+                        height={500}
+                    />
+                </section>
 
-        {/* Product Name */}
-        <h1 className="text-center text-[24px] font-bold text-slate-900 hover:text-slate-800 hover:underline">
-          {product.title}
-        </h1>
-      </Link>
-      {/* Price */}
-      <table className="w-full">
-        <thead>
-          <tr>
-            <th className="text-center">Normal</th>
-            <th className="text-center">Combo</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="text-center">${product.prices[0]}.00</td>
-            <td className="text-center">${product.prices[1]}.00</td>
-          </tr>
-        </tbody>
-      </table>
+                {/* Product Name */}
+                <h1 className="text-center text-[24px] font-bold text-slate-900">
+                    {product.title}
+                </h1>
+            </div>
+            {/* Price */}
+            <div className="font-semibold text-gray-800">
+                ${product.prices[0]}.00
+            </div>
 
-      {/* Description */}
-      <p className="mt-2 text-center sm:text-left text-[16px] text-slate-800">
-        {product.desc.length > 100
-          ? `${product.desc.substring(0, 97)}...`
-          : product.desc}
-      </p>
-    </div>
-  );
+            <Link
+                passHref
+                href={`/products/${product._id}`}
+                className="bg-red-700 hover:bg-red-800 ease-linear duration-200 text-white w-full text-center py-1 rounded-sm text-lg font-semibold mt-4"
+            >
+                Add
+            </Link>
+        </div>
+    );
 };
 
 export default ProductCard;
