839bf54d7b992d12174d88e19bbaa373a43a49b2
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-01 13:03:30 -0700

    Downsized text and updated margins

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,23 +1,23 @@
 const Footer = () => {
     return (
         // Container
-        <div className="h-auto bg-zinc-900 flex pt-10 w-full justify-center mb-16 sm:mb-10">
+        <div className="h-auto bg-zinc-900 flex sm:pt-10 w-full justify-center mb-36 sm:mb-12">
             {/* Contact */}
             <div className="flex flex-col sm:flex-row flex-2 relative text-gray-200 p-12 justify-between text-center sm:text-left">
                 {/* Motto */}
                 <div className="flex flex-1 pb-10 sm:py-0">
-                    <h1 className="text-[32px] font-bold text-gray-100">ALWAYS SERVING THE BEST FOODS</h1>
+                    <h1 className="text-2xl sm:text-[32px] font-bold text-gray-100">ALWAYS SERVING THE BEST FOODS</h1>
                 </div>
                 {/* Locations */}
                 <div className="flex-2 py-0 sm:px-5">
-                    <h1 className="text-[28px] sm:text-[18px] text-amber-300 font-bold pb-5">FIND OUR RESTAURANTS</h1>
-                    <p className="text-[20px] sm:text-[16px] pb-5">
+                    <h1 className="text-xl sm:text-[18px] text-amber-300 font-bold pb-5">FIND OUR RESTAURANTS</h1>
+                    <p className="text-[16px] pb-5">
                         123 Addy Way,
                         <br />
                         Notacity, 98765
                         <br /> (123) 456-7890
                     </p>
-                    <p className="text-[20px] sm:text-[16px] pb-5">
+                    <p className="text-[16px] pb-5">
                         1234 Addy Way,
                         <br />
                         Notacity, 98765
@@ -27,12 +27,12 @@ const Footer = () => {
                 
                 {/* Hours */}
                 <div className="flex-1 py-0 sm:px-5">
-                    <h1 className="text-[28px] sm:text-[18px] text-amber-300 font-bold pb-5">WORKING HOURS</h1>
-                    <p className="text-[20px] sm:text-[16px] pb-5">
+                    <h1 className="text-xl sm:text-[18px] text-amber-300 font-bold pb-5">WORKING HOURS</h1>
+                    <p className="text-[16px] pb-5">
                         MONDAY THROUGH FRIDAY
                         <br /> 9:00 - 24:00
                     </p>
-                    <p className="text-[20px] sm:text-[16px] pb-5">
+                    <p className="text-[16px] pb-5">
                         SATURDAY &amp; SUNDAY
                         <br /> 12:00 - 21:00
                     </p>
