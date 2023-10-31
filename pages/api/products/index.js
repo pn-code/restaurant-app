1b74d651989a2f8a8dfa92d586d89ed87f947f0e
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-30 21:08:00 -0700

    Updated route to give status 201

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     if (method === "POST") {
         try {
             const product = await Product.create(req.body);
-            res.status(200).json(product);
+            res.status(201).json(product);
         } catch (err) {
             res.status(500).json(err);
         }
