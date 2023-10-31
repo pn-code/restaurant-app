cd937c188d08570c2fbfa851f5ed9455c614b3a5
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-30 19:40:37 -0700

    Updated colors and fonts for hero

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 
 export default function HeroSection() {
     return (
-        <section className="bg-[#0f0f0f] flex flex-col lg:flex-row">
+        <section className="bg-[#0f0f0f]/60 flex flex-col lg:flex-row">
             <section className="flex-[2]">
                 <Image src="/assets/hero-img.png" height={600} width={1000} />
             </section>
 
             <section className="p-2 text-center lg:mr-[2%] flex-[1.5] py-20 text-white flex justify-center items-center w-full flex-col text-2xl font-bold gap-8 lg:text-4xl lg:gap-10">
-                <div className="text-3xl lg:text-5xl">A Legacy of Flavor: </div>
-                <div>HANDCRAFTED BURGERS &amp; FRIES</div>
-                <div className="text-2xl lg:text-4xl">since 1926</div>
+                <div className="text-2xl lg:text-4xl">A Legacy of Flavor: </div>
+                <div className="text-xl lg:text-3xl">HANDCRAFTED BURGERS &amp; FRIES</div>
+                <div className="text-xl lg:text-2xl">SINCE 1926</div>
                 <Link
                     passHref
                     href="/menu"
