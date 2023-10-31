d7f19841485a1d6771eb42345e72b04b7b904ab0
Philip Nguyen <philipnguyen953@gmail.com>
2023-10-30 21:14:14 -0700

    Updated styles and added image container cover

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -3,40 +3,37 @@ import Image from "next/image";
 import Link from "next/link";
 
 const ProductCard = ({ product }) => {
-    return (
-        // Container
-        <div className="bg-gray-50 rounded-md shadow-md border-2 border-gray-300 w-[100%] sm:w-[50%] md:w-[33%] xl:w-[20%] flex flex-col items-center justify-center py-5 px-10">
-            {/* Product Image */}
-            <div className="flex flex-col gap-2">
-                <section className="w-[200px] h-[200px] flex justify-center items-center">
-                    <Image
-                        className="rounded-md"
-                        src={product.image}
-                        alt={product.title}
-                        width={500}
-                        height={500}
-                    />
-                </section>
+  return (
+    // Container
+    <div className="bg-gray-50 rounded-md shadow-md border-2 border-gray-300 w-[100%] md:max-w-[320px] flex flex-col items-center gap-3 py-5 px-10">
+      {/* Product Image */}
+      <section className="relative w-[200px] h-[180px]">
+        <Image
+          className="w-full h-full object-cover"
+          src={product.image}
+          alt={product.title}
+          width={500}
+          height={500}
+        />
+      </section>
 
-                {/* Product Name */}
-                <h1 className="text-center text-[18px] font-bold text-slate-900 pb-2">
-                    {product.title}
-                </h1>
-            </div>
-            {/* Price */}
-            <div className="font-semibold text-gray-800">
-                ${product.prices[0]}.00
-            </div>
+      {/* Title */}
+      <h1 className="text-center text-[18px] font-bold text-slate-900">
+        {product.title}
+      </h1>
 
-            <Link
-                passHref
-                href={`/products/${product._id}`}
-                className="bg-red-700 hover:bg-red-800 ease-linear duration-200 text-white w-full text-center py-1 rounded-sm text-lg font-semibold mt-4"
-            >
-                Add
-            </Link>
-        </div>
-    );
+      {/* Price */}
+      <div className="font-semibold text-gray-800">${product.prices[0]}.00</div>
+
+      <Link
+        passHref
+        href={`/products/${product._id}`}
+        className="bg-red-700 hover:bg-red-800 ease-linear duration-200 text-white w-full text-center py-1 rounded-sm text-lg font-semibold mt-4"
+      >
+        Add
+      </Link>
+    </div>
+  );
 };
 
 export default ProductCard;
